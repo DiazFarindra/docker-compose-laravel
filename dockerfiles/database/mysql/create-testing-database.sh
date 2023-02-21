678690b81150b5ac0feac335857175b60c0d5d34
DiazFarindra <farindrad@gmail.com>
2023-02-21 20:33:26 +0700

    fix: :bug: fix sql config

--- dockerfiles/database/mysql/create-testing-database.sh
+++ dockerfiles/database/mysql/create-testing-database.sh
@@ -2,5 +2,6 @@
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS testing;
-    GRANT ALL PRIVILEGES ON \`testing%\`.* TO 'testing'@'%';
+    CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+    GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$DB_USER'@'%';
 EOSQL
